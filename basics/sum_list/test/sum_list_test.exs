0d107260ff5215264b3896bb2b0544d7bc3ff718
bertdev <herbertmatos10@hotmail.com>
2021-12-22 19:33:28 -0300

    created a function to sum a list using enum

--- basics/sum_list/test/sum_list_test.exs
+++ basics/sum_list/test/sum_list_test.exs
@@ -11,4 +11,15 @@ defmodule SumListTest do
       assert response == expected_response
     end
   end
+
+  describe "call_enum/1" do
+    test "returns the list sum" do
+      list = [1, 2, 3]
+
+      response = SumList.call_enum(list)
+      expected_response = 6
+
+      assert response == expected_response
+    end
+  end
 end
